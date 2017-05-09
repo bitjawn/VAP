d227478bf93588b062f6ea29bd7513a269002be0
bitjawn <ricwalker59@gmail.com>
2017-05-09 12:00:40 -0400

    Initial commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
     <h2>{{msg}}</h2>
      <div class="row v-scroll">
         <img src="graphics/background4.png" class="about">
-        <div class="left-floater border">
+        <div class="left-floater">
             <h4>Lorem ipsum dolor sit<div class="subheader">Amet aliquam faucibus ac</div></h4>
             <p>
                 Elementum euismod aenean fermentum. Ligula luctus consequat proin. Mollis bibendum fringilla eu non sed in ac. Sem in nonummy mauris. At id urna vel dolor ipsum tempus mus. Gravida torquent neque dolor. Fusce a tortor vulputate. Habitasse cras ultrices aliquet sagittis augue dui fusce consequat nullam libero et ipsum vel consequat dictumst.
@@ -12,7 +12,7 @@
                 Et pellentesque at lacus. Est turpis quis proin eget, scelerisque sociis penatibus tellus massa, justo mauris nobis vestibulum. Wisi sapien sagittis id vel ut odio.
             </p>
         </div>
-        <div class="right-floater border">
+        <div class="right-floater">
             <h4>Accumsan rutrum arcu quis</h4>
             <p>
                 In vulputate nam aliquam. Porttitor suspendisse ultricies sit. Felis eu ac felis. Pharetra omnis eget ornare odio wisi metus risus morbi semper nulla donec vitae in.
